9ca6baf312e2b623c764c55753d8a7e9326a3535
_michal <sz-m@users.noreply.github.com>
2016-11-23 09:36:32 +0000

    change NULL to nullptr

--- main.cpp
+++ main.cpp
@@ -14,7 +14,7 @@ typedef vector<Shape*> Collection;
 
 bool sortByArea(Shape* first, Shape* second)
 {
-    if(first == NULL || second == NULL)
+    if(first == nullptr || second == nullptr)
     {
         return false;
     }
@@ -43,7 +43,7 @@ void printCollectionElements(const Collection& collection)
 {
     for(Collection::const_iterator it = collection.begin(); it != collection.end(); ++it)
     {
-        if(*it != NULL)
+        if(*it != nullptr)
         {
             (*it)->print();
         }
@@ -60,7 +60,7 @@ void printAreas(const Collection& collection)
     std::vector<std::thread> threads;
     for(vector<Shape*>::const_iterator it = collection.begin(); it != collection.end(); ++it)
     {
-        if(*it != NULL)
+        if(*it != nullptr)
         {
             std::thread th(printArea, (*it)->getName(), (*it)->getArea());
             threads.push_back(std::move(th));
@@ -77,7 +77,7 @@ void findFirstShapeMatchingPredicate(const Collection& collection,
                                      std::string info)
 {
     Collection::const_iterator iter = std::find_if(collection.begin(), collection.end(), predicate);
-    if(*iter != NULL)
+    if(*iter != nullptr)
     {
         cout << std::endl << "First shape matching predicate: " << info << endl;
         (*iter)->print();
@@ -100,7 +100,7 @@ public:
     Shape* pop()
     {
         // TODO
-        return NULL;
+        return nullptr;
     }
 };
 
@@ -112,7 +112,7 @@ void runQueue()
     while(running)
     {
         Shape * shape = g_queue.pop();
-        if(shape == NULL)
+        if(shape == nullptr)
         {
             std::cout << "Queue received NULL, finishing loop" << std::endl;
             break;
@@ -137,7 +137,7 @@ int main()
     Collection shapes;
     shapes.push_back(new Circle(2.0));
     shapes.push_back(new Circle(3.0));
-    shapes.push_back(NULL);
+    shapes.push_back(nullptr);
     shapes.push_back(new Circle(4.0));
     shapes.push_back(new Rectangle(10.0, 5.0));
     shapes.push_back(new Square(3.0));
@@ -165,5 +165,6 @@ int main()
     queueThread.join();
 
     return 0;
+
 }
 
